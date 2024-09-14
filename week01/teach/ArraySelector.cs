efcb2ad718df71b60d960df9053aba062ed06f8b
shumwaynate <nateshumway1@gmail.com>
2024-09-14 15:38:14 -0600

    first

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,23 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var results = new int[select.Length];
+        int index1 = 0;
+        int index2 = 0;
+
+        for(int i = 0; i < select.Length; i++)
+        {
+            if(select[i] == 1)
+            {
+                results[i] = list1[index1];
+                index1++;
+            }else if(select[i] == 2)
+            {
+                results[i] = list2[index2];
+                index2++;
+            }
+            // Console.WriteLine("item " + i+1 + " is " + results[i]);
+        }
+        return results;
     }
 }
